8b202ce6d717cc1b14988b325fd93bec030cfb44
Muky-dev <69209901+Muky-dev@users.noreply.github.com>
2021-08-20 20:01:39 -0300

    Fixed routes order

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,8 @@ import express, { Express } from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
 
-import router from './routes'
+import favoriteRoutes from './routes/favorite'
+import rootRoutes from './routes/index'
 
 const app: Express = express()
 
@@ -11,7 +12,8 @@ const PORT: string | number = process.env.PORT || 4000
 app.use(cors())
 app.use(express.urlencoded({ limit: '20mb', extended: false }))
 app.use(express.json())
-app.use(router)
+app.use('/favorite', favoriteRoutes)
+app.use('/', rootRoutes)
 
 const mongoURI: string = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vabk6.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
 
