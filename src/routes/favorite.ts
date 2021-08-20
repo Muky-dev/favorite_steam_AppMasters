63ae5ecebdb33f1778f184618a8e3c9cc1c16a3e
Muky-dev <69209901+Muky-dev@users.noreply.github.com>
2021-08-20 20:02:38 -0300

    Favorite routes (almost completed)

--- src/routes/favorite.ts
+++ src/routes/favorite.ts
@@ -1,17 +1,34 @@
 import Router, { Request, Response } from 'express'
+import Favorite from '../models/Favorite'
 
 const router = Router()
 
-router.get('/favorite')
+router.get('/', async (req: Request, res: Response): Promise<void> => {
+    const userHash = req.headers['user-hash']
+    try {
+        const data = await Favorite.find({ userHash: userHash })
+        res.status(200).json(data)
+    } catch (err) {
+        res.status(500).json({ error: err })
+    }
+})
 
-router.post('/favorite', async (req: Request, res: Response): Promise<void> => {
+router.post('/', async (req: Request, res: Response): Promise<void> => {
     try {
         const { appid, rating } = req.body
+        const userHash = req.headers['user-hash']
+        const favoriteInstance = new Favorite({
+            appid: appid,
+            rating: rating,
+            userHash: userHash,
+        }).save()
+
+        res.json({ message: 'Favorito criado', data: favoriteInstance })
     } catch (err) {
         res.status(400).json({ error: err })
     }
 })
 
-router.delete('/favorite/:appid')
+router.delete('/:appid')
 
 export default router
