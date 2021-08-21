7da1f66c81904b6fc7f182b6abd9fb8c30a57f2f
Muky-dev <69209901+Muky-dev@users.noreply.github.com>
2021-08-21 13:38:56 -0300

    Added cache function

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,16 +1,23 @@
 import Router, { Request, Response } from 'express'
+import cache from 'memory-cache'
 import axios, { AxiosResponse } from 'axios'
 import { IApplist, IUniqueApp } from '../types/axios'
 
 const router = Router()
 
 router.get('/', async (req: Request, res: Response): Promise<void> => {
+    const baseUrl: string =
+        'https://api.steampowered.com/ISteamApps/GetAppList/v0002/?format=json'
     try {
-        const { data }: AxiosResponse<IApplist> = await axios.get(
-            'https://api.steampowered.com/ISteamApps/GetAppList/v0002/?format=json',
-        )
-        const appArray = data.applist.apps.slice(0, 10)
-        res.status(200).json(appArray)
+        const cached = cache.get(baseUrl)
+        if (cached) {
+            res.status(200).json(cached)
+        } else {
+            const { data }: AxiosResponse<IApplist> = await axios.get(baseUrl)
+            const appArray = data.applist.apps.slice(0, 100)
+            cache.put(baseUrl, appArray)
+            res.status(200).json(appArray)
+        }
     } catch (err) {
         res.status(500).json({ error: err })
     }
@@ -18,15 +25,22 @@ router.get('/', async (req: Request, res: Response): Promise<void> => {
 
 router.get('/:id', async (req: Request, res: Response): Promise<void> => {
     const { id } = req.params
+    const requestApiUrl: string = `https://store.steampowered.com/api/appdetails?appids=${id}`
     try {
-        const requestApiUrl: string = `https://store.steampowered.com/api/appdetails?appids=${id}`
-        const { data }: AxiosResponse<IUniqueApp> = await axios.get(
-            requestApiUrl,
-        )
-        if (!data[id].success) {
-            throw "Can't find this app"
+        const cached = cache.get(requestApiUrl)
+        if (cached) {
+            res.status(200).json(cached)
+        } else {
+            const { data }: AxiosResponse<IUniqueApp> = await axios.get(
+                requestApiUrl,
+            )
+            if (!data[id].success) {
+                throw "Can't find this app"
+            }
+            const dataResponse = data[id].data
+            cache.put(requestApiUrl, dataResponse)
+            res.status(200).json(dataResponse)
         }
-        res.status(200).json(data[id].data)
     } catch (err) {
         res.status(400).json({ error: err })
     }
