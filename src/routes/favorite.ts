2da69034088a110c5e225d8d4e34994b622860e7
Muky-dev <69209901+Muky-dev@users.noreply.github.com>
2021-08-22 14:24:12 -0300

    Great improves about error handling

--- src/routes/favorite.ts
+++ src/routes/favorite.ts
@@ -1,5 +1,6 @@
 import Router, { Request, Response } from 'express'
 import axios, { AxiosResponse } from 'axios'
+
 import Favorite from '../models/Favorite'
 import { IUniqueApp } from '../types/axios'
 import { IFavorite } from '../types/favorite'
@@ -17,6 +18,7 @@ router.get('/', async (req: Request, res: Response): Promise<void> => {
         if (favoriteArray.length == 0) {
             throw "This user don't have favorites"
         }
+
         res.status(200).json(favoriteArray)
     } catch (err) {
         res.status(400).json({ error: err })
@@ -24,32 +26,40 @@ router.get('/', async (req: Request, res: Response): Promise<void> => {
 })
 
 // Create new favorite
-router.post('/', async (req: Request, res: Response): Promise<void> => {
-    const { appid, rating } = req.body
+router.post('/:appid', async (req: Request, res: Response): Promise<void> => {
+    const { rating } = req.body
+    const { appid } = req.params
     const user = req.user
+
+    const requestApiUrl = `https://store.steampowered.com/api/appdetails?appids=${appid}`
     try {
         const { data }: AxiosResponse<IUniqueApp> = await axios.get(
-            `https://store.steampowered.com/api/appdetails?appids=${appid}`,
+            requestApiUrl,
         )
-        if (!data[appid].success) {
+        // verify if the app exists and if successfully had a response
+        if (!data[appid].success || !appid) {
             throw 'Please insert a valid appid'
         }
 
-        const fixed = () => {
-            let parsedRating = parseInt(rating)
-            if (parsedRating > 5) {
-                parsedRating = 5
-            } else if (parsedRating < 0) {
-                parsedRating = 0
-            }
-            return parsedRating
+        const parsedRating = parseInt(rating)
+        if (parsedRating > 5 || parsedRating < 0) {
+            throw 'Please provide a rating between 0 and 5'
         }
-        const newRating = fixed()
+        const parsedAppId = parseInt(appid)
+
+        const favoriteExists: IFavorite | null = await Favorite.findOne({
+            appid: parsedAppId,
+            userHash: user,
+        })
+
+        if (favoriteExists) {
+            throw 'This favorite already exists'
+        }
 
         const favoriteInstance: IFavorite = await Favorite.create({
             appid: appid,
             userHash: user,
-            rating: newRating,
+            rating: parsedRating,
             data: data[appid].data,
         })
 
@@ -58,10 +68,11 @@ router.post('/', async (req: Request, res: Response): Promise<void> => {
             data: {
                 appid: favoriteInstance.appid,
                 rating: favoriteInstance.rating,
+                user: user,
             },
         })
     } catch (err) {
-        res.status(500).json({ error: err })
+        res.status(400).json({ error: err })
     }
 })
 
@@ -71,13 +82,17 @@ router.delete('/:appid', async (req: Request, res: Response): Promise<void> => {
     const user = req.user
     try {
         const parsedAppId = parseInt(appid)
-        await Favorite.findOneAndDelete({
+        const favoriteExists = await Favorite.findOne({
             userHash: user,
             appid: parsedAppId,
         })
+        if (!favoriteExists) {
+            throw "this favorite doesn't exist"
+        }
+        favoriteExists.delete()
         res.status(200).json({ message: 'Favorite deleted' })
     } catch (err) {
-        res.status(500).json({ error: err })
+        res.status(400).json({ error: err })
     }
 })
 
